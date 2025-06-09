a0753f7abe2688893eb93e92b2932e331e63eaff
saravanan ss <itssaravana1999@gmail.com>
2025-06-09 22:46:24 +0530

    Scroll trigger

--- GSAP/script.js
+++ GSAP/script.js
@@ -1,86 +1,26 @@
-gsap.from("h1",{
-    y:30,
-color:"red",
-opacity:0,
-duration:2,
-delay:1,
-stagger:1,
-repeat:Infinity,
-yoyo:true
+
+gsap.to("#page2 h1",{
+    x:-1200,
+    scrollTrigger:{
+        Trigger:"#page2,h1",
+        scroller:"body",
+        markers:true,
+        start:"top -90%",
+        end:"top -110%",
+        scrub:2
+    }
 })
 
-gsap.from("h3",{
-    x:-100,
-color:"green",
-opacity:1,
-duration:4,
-delay:1.5,
-stagger:1
-})
+gsap.to("#page2 h1",{
+    Transform:"translateX(-200%)",
+    scrollTrigger:{
+        Trigger:"#page2 h1",
+        scroller:"body",
+        markers:true,
+        start:"top -90%",
+        end:"top -110%",
+        scrub:1,
+        pin:true
 
-gsap.to("#box1",{
-    x:900,
-    duration:6,
-    Delay:3,
-    rotate:360,
-    backgroundColor:"gold",
-    borderRadius:"50%",
-    scale:.2,
-    repeat:-1,
-    yoyo:true
-})
-
-
-var tl = gsap.timeline()
-
-tl.from("h2",{
-   
-    y:-20,
-    opacity:0,
-    duration:1.5,
-    delay:1,
-    stagger:1,
-    yoy:true,
-   
-
-})
-tl.from("#part2",{
-   
-    y:-20,
-    opacity:0,
-    duration:1,
-    delay:0.5,
-    stagger:1
-
-})
-
-tl.to("#box2",{
-x:900,
-duration:6,
-dealy:1,
-rotate:360,
-backgroundColor:"blue",
-borderRadius:"60%",
-scale:.8,
-repeat:Infinity,
-yoyo:true
-})
-gsap.from("#page2 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
-    rotate:360  
- 
-})
-gsap.from("#page3 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
- 
-})
-gsap.from("#page4 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
- 
+    }
 })
